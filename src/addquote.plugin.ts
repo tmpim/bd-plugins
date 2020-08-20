74e0bfeb34aa8b844f51d4142a83655cfe3fc021
emmachase <its.emma.chase@gmail.com>
2020-08-19 19:46:29 -0600

    feat: add AddQuote multi-selection

--- src/addquote.plugin.ts
+++ src/addquote.plugin.ts
@@ -5,10 +5,17 @@
 
 import { BdPlugin } from "../types/BdPlugin";
 import { CancelPatch } from "../types/BdApi";
+import { Discord } from "../types/DiscordTypes";
 
 class AddQuote implements BdPlugin {
     cancelRenderPatch: CancelPatch;
-    cancelMenuActionsPatch: CancelPatch;
+
+    TextBoxModule: any;
+    nativeTextBox: React.ComponentClass;
+
+    RichDocumentModule: {
+        deserialize(s: string): any;
+    };
 
     Tooltip: any;
     ButtonComponents: {
@@ -30,7 +37,20 @@ class AddQuote implements BdPlugin {
             invalidEmojis: Array<any>,
             validNonShortcutEmojis: Array<any>
         }): Promise<any>;
-    }
+    };
+
+    MessageStore: {
+        getMessages(channelId: string): {
+            get(messageId: string): Discord.Message | null;
+
+            // Gets the message immediately after the supplied msg, if it exists
+            getAfter(messageId: string): Discord.Message | null;
+
+            last(): Discord.Message | null;
+
+            toArray(): Discord.Message[];
+        }
+    };
 
     getName(): string { return "AddQuote"; }
     getDescription(): string { return "Adds an 'Add Quote' button to message action bar."; }
@@ -54,14 +74,20 @@ class AddQuote implements BdPlugin {
         }
 
         this.MessageUtils = BdApi.findModuleByProps("sendMessage");
+        this.MessageStore = BdApi.findModuleByProps("getMessage", "getMessages");
+        this.RichDocumentModule = BdApi.findModuleByProps("deserialize", "serialize");
 
         this.performHookPatch();
+        this.injectCSS();
     }
 
     stop(): void {
+        this.TextBoxModule.default = this.nativeTextBox;
         this.cancelRenderPatch();
+        this.cleanupCSS();
     }
 
+    currentTextBox: any;
     performHookPatch() {
         const PanelModule = BdApi.findModuleByProps("useConnectedUtilitiesProps");
         this.cancelRenderPatch = BdApi.monkeyPatch(PanelModule.default, "type", {
@@ -71,6 +97,52 @@ class AddQuote implements BdPlugin {
                 MenuActionsInstance.type = this.performMenuPatch(MenuActions);
             }
         });
+
+        this.TextBoxModule = BdApi.findModule(x => x && x.default && x.default.displayName === "ChannelEditorContainer");
+        this.nativeTextBox = this.TextBoxModule.default;
+
+        const self = this;
+        const nTB = this.nativeTextBox as any;
+        this.TextBoxModule.default = class ProxyTextBox extends nTB {
+            // tbInst: any;
+            originalMethod: any;
+
+            constructor(props: any) {
+                // debugger;
+                super(props);
+                this.originalHandleTabOrEnter = this.handleTabOrEnter;
+                // this.handleTabOrEnter = function(oldThis) {
+                //     this.handleInsertTextOverride.bind(this);
+                // }
+                this.handleTabOrEnter = this.handleTabOrEnterOverride.bind(this);
+            }
+
+            render() {
+                self.currentTextBox = this;
+
+                const channel = this.props.channel;
+                if (this.props.textValue.startsWith("!addquote")
+                &&  channel.guild_id === "591488795040546818") {
+                    self.parseRange(channel.id, this.props.textValue);
+                } else {
+                    const channel = this.props.channel.id;
+                    if (self.selectedMessages[channel]) {
+                        self.selectedMessages[channel] = null;
+                        self.clearClasses();        
+                    }
+                }
+
+                return super.render();
+            }
+
+            handleTabOrEnterOverride() {
+                const channel = this.props.channel.id;
+                self.selectedMessages[channel] = null;
+                self.clearClasses();
+
+                return this.originalHandleTabOrEnter(arguments);
+            }
+        }
     }
 
     filterProperties(obj: any, props: string[]) {
@@ -131,13 +203,186 @@ class AddQuote implements BdPlugin {
         ));
     }
 
+    sortSnowflakes(...snowflakes: string[]) {
+        return snowflakes
+            .map(BigInt)
+            .sort((a, b) => Number(a - b))
+            .map(String);
+    }
+
+    getMessagesFromRange(channelId: string): Discord.Message[] {
+        const range = this.selectedMessages[channelId];
+        if (!range) return;
+
+        const channel = this.MessageStore.getMessages(channelId);
+        const messages: Discord.Message[] = [];
+
+        const [begin, end] = this.sortSnowflakes(range.first, range.second);
+
+        let tempMsg = channel.get(begin);
+        while (true) {
+            if (!tempMsg) break;
+
+            messages.push(tempMsg);
+            if (tempMsg.id === end) break;
+
+            tempMsg = channel.getAfter(tempMsg.id);
+        }
+
+        return messages;
+    }
+
+    renderSelectedMessages(messages: Discord.Message[]) {
+        // First clear out the old selection
+        this.clearClasses();
+
+        messages.sort((a, b) => Number(BigInt(a.id) - BigInt(b.id)));
+
+        // Now add the class to all the new messages
+        let lastNode: HTMLElement;
+        for (const message of messages) {
+            const messageNode = document.getElementById("chat-messages-" + message.id);
+            if (messageNode instanceof HTMLElement) {
+                messageNode.classList.add("addquote-selected");
+
+                if (messageNode.classList.contains("da-groupStart") || !lastNode) {
+                    lastNode?.classList.add("addquote-endGroup");
+                    messageNode.classList.add("addquote-startGroup");
+                }
+
+                lastNode = messageNode;
+            }
+        }
+
+        lastNode?.classList.add("addquote-endGroup");
+    }
+
+    static RANGE_RE = /^!addquote (\d{4,})(\+\d+)?$/;
+    static REL_RANGE_RE = /^!(?:addquote |addquote)(?:\~(\d+))?(\+\d+)?$/;
+    parseRange(channel: string, text: string) {
+        const fail = () => {
+            this.selectedMessages[channel] = null;
+            this.clearClasses();
+        }
+
+        let snowflake, length;
+
+        const messages = this.MessageStore.getMessages(channel);
+
+        let matchResult = AddQuote.RANGE_RE.exec(text);
+        if (matchResult) {
+            [, snowflake, length] = matchResult;
+        } else {
+            matchResult = AddQuote.REL_RANGE_RE.exec(text);
+            if (matchResult) {
+                let relative;
+                [, relative, length] = matchResult;
+                if (length && !relative) {
+                    length = +length - 1;
+                }
+
+                const cached = messages.toArray();
+                const msg = cached[cached.length - +(relative || "1") - +(length || "0")];
+                if (msg) {
+                    snowflake = msg.id;
+                } else {
+                    return fail();
+                }
+            } else {
+                return fail();
+            }
+        }
+
+        let firstMessage = messages.get(snowflake);
+        if (!firstMessage) {
+            this.selectedMessages[channel] = null;
+            this.clearClasses();
+            return;
+        };
+
+        let endMessage = firstMessage;
+        if (length) {
+            for (let i = 0; i < +length; i++) {
+                const nextMsg = messages.getAfter(endMessage.id);
+                if (nextMsg) endMessage = nextMsg;
+                else break;
+            }
+        }
+
+        const currentRange = this.selectedMessages[channel];
+        if (currentRange) {
+            if (currentRange.first === snowflake) {
+                this.selectedMessages[channel] = {
+                    first: snowflake,
+                    second: endMessage.id
+                };
+            } else {
+                this.selectedMessages[channel] = {
+                    first: endMessage.id,
+                    second: snowflake
+                };
+            }
+        } else {
+            this.selectedMessages[channel] = {
+                first: snowflake,
+                second: endMessage.id
+            };
+        }
+
+        const messageRange = this.getMessagesFromRange(channel);
+        this.renderSelectedMessages(messageRange);
+    }
+
+    formatRange(rangeSize: number, range: { first: string, second: string }) {
+        const [beginMsg] = this.sortSnowflakes(range.first, range.second);
+        if (rangeSize === 1) {
+            return beginMsg;
+        } else {
+            return `${beginMsg}+${rangeSize - 1}`;
+        }
+    }
+
+    proccessAddQuote(channel: Discord.Channel, message: Discord.Message) {
+        if (channel.guild_id !== "591488795040546818") {
+            return BdApi.showToast(
+                "Can't use addquote in a non tmpim guild", 
+                { type: "error" }
+            );
+        }
+
+        let newRange: AddQuote["selectedMessages"][keyof AddQuote["selectedMessages"]];
+        if (!this.selectedMessages[channel.id]) {
+            this.selectedMessages[channel.id] = newRange = {
+                first: message.id,
+                second: message.id
+            }
+        } else {
+            newRange = this.selectedMessages[channel.id];
+            newRange.first = newRange.second;
+            newRange.second = message.id;
+        }
+
+        const messageRange = this.getMessagesFromRange(channel.id);
+        this.renderSelectedMessages(messageRange);
+
+        const currentTextBox = this.currentTextBox;
+        if (currentTextBox) {
+            const quoteRange = this.formatRange(messageRange.length, newRange);
+            const quoteText = "!addquote " + quoteRange;
+
+            const editor = currentTextBox.ref.current.editorRef;
+            editor.moveToRangeOfDocument().insertText(quoteText);
+            currentTextBox.focus();
+        }
+    }
+
+    // Record of channel id -> message range
+    selectedMessages: Record<string, { first: string, second: string }> = {};
+
     overiddenMenuItems: any;
     performMenuPatch(MenuActions: React.FunctionComponent<any>): Function {
         if (this.overiddenMenuItems) return this.overiddenMenuItems;
-
-        const renderIconButton = this.renderIconButton.bind(this);
-        const QuoteIcon = this.QuoteIcon;
-        const MessageUtils = this.MessageUtils;
+        const self = this;
 
         this.overiddenMenuItems = class NewMenu extends BdApi.React.Component {
             render() {
@@ -145,27 +390,13 @@ class AddQuote implements BdPlugin {
                     const renderValue = MenuActions(props);
                     const children: any[] = renderValue.props.children;
                     children.splice(children.length - 2, 0, 
-                        renderIconButton({
+                        self.renderIconButton({
                             channel: props.channel,
                             message: props.message,
                             disabled: false,
                             label: "Add Quote",
-                            icon: QuoteIcon,
-                            onClick: (channel: any, message: any) => {
-                                if (channel.guild_id !== "591488795040546818") {
-                                    return BdApi.showToast(
-                                        "Can't use addquote in a non tmpim guild", 
-                                        { type: "error" }
-                                    );
-                                }
-
-                                MessageUtils.sendMessage(channel.id, {
-                                    content: `!addquote ${message.id}`,
-                                    tts: false,
-                                    invalidEmojis: new Array(),
-                                    validNonShortcutEmojis: new Array()
-                                  })
-                            }
+                            icon: self.QuoteIcon,
+                            onClick: self.proccessAddQuote.bind(self)
                         }));
 
                     return renderValue;
@@ -174,6 +405,53 @@ class AddQuote implements BdPlugin {
         }
 
         return this.overiddenMenuItems;
+    }
+
+    cssNode: string = "pluginAddQuoteCSS"
+    injectCSS() {
+        // .da-cozyMessage
+        BdApi.injectCSS(this.cssNode, `
+            .addquote-selected {
+                background: rgba(250, 166, 26, 0.3) !important;
+            }
+
+            .addquote-selected:hover {
+                background: rgba(250, 166, 26, 0.3) !important;
+            }
+
+            .addquote-startGroup.da-cozy {
+                border-radius: 12px 12px 0 0;
+            }
+
+            .addquote-endGroup.da-cozy {
+                border-radius: 0 0 12px 12px;
+            }
+
+            .addquote-startGroup.addquote-endGroup.da-cozy {
+                border-radius: 12px;
+            }
+        `);
+    }
+
+    cleanupCSS() {
+        this.clearClasses();
+        BdApi.clearCSS(this.cssNode);
+    }
+
+    clearClasses() {
+        const oldSelection = document.getElementsByClassName("addquote-selected");
+
+        // Because HTMLCollections update themselves as you iterate
+        const collection = [];
+        for (const x of oldSelection) collection.push(x);
+
+        for (const message of collection) {
+            if (message instanceof HTMLElement) {
+                message.classList.remove("addquote-selected");
+                message.classList.remove("addquote-startGroup")
+                message.classList.remove("addquote-endGroup")
+            }
+        }
     }
 }
 
---
+++ types/DiscordTypes.d.ts
@@ -0,0 +1,85 @@
+export namespace Discord {
+    export interface Message {
+        id: string;
+        type: number;
+        channel_id: string;
+        author: Author;
+        colorString: string;
+        nick?: null;
+        content: string;
+        attachments?: (null)[] | null;
+        embeds?: (null)[] | null;
+        mentions?: (null)[] | null;
+        mentionRoles?: (null)[] | null;
+        mentionChannels?: (null)[] | null;
+        mentioned: boolean;
+        pinned: boolean;
+        mentionEveryone: boolean;
+        tts: boolean;
+        codedLinks?: (null)[] | null;
+        giftCodes?: (null)[] | null;
+        timestamp: string;
+        editedTimestamp?: null;
+        state: string;
+        nonce: string;
+        blocked: boolean;
+        call?: null;
+        bot: boolean;
+        webhookId?: null;
+        reactions?: (null)[] | null;
+        application?: null;
+        activity?: null;
+        messageReference?: null;
+        flags: number;
+        isSearchHit: boolean;
+    }
+
+    export interface Author {
+        id: string;
+        username: string;
+        usernameNormalized: string;
+        discriminator: string;
+        avatar: string;
+        email?: null;
+        verified: boolean;
+        bot: boolean;
+        system: boolean;
+        mfaEnabled: boolean;
+        mobile: boolean;
+        desktop: boolean;
+        flags: number;
+        publicFlags: number;
+        phone?: null;
+    }
+
+    export interface Channel {
+        id: string;
+        type: number;
+        name: string;
+        topic: string;
+        position: number;
+        guild_id: string;
+        recipients?: (null)[] | null;
+        rawRecipients?: (null)[] | null;
+        permissionOverwrites: PermissionOverwrites;
+        bitrate: number;
+        userLimit: number;
+        ownerId?: null;
+        icon?: null;
+        application_id?: null;
+        nicks: Nicks;
+        nsfw: boolean;
+        parent_id: string;
+        memberListId?: null;
+        rateLimitPerUser: number;
+        originChannelId?: null;
+        lastMessageId?: null;
+        lastPinTimestamp?: null;
+    }
+
+      export type PermissionOverwrites = 
+        {[p: string]: {id: string, type: number, allow: any, deny: any}};
+
+      
+      export interface Nicks {}
+}
